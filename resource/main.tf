292cf98433b7779f5a2ed15cf153406d83dec30f
satya <satya.bhimuni123@gmail.com>
2023-11-24 11:20:35 +0530

    project

---
+++ resource/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.example.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "venkata"
+  }
+}
+
+data "aws_ami" "example" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
