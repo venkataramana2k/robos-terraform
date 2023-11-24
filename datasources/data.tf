0dfb19098b60eaab9d7f92699b0fdd225054e343
satya <satya.bhimuni123@gmail.com>
2023-11-24 11:05:54 +0530

    project

--- datasources/data.tf
+++ datasources/data.tf
@@ -12,3 +12,11 @@ output "t2_micro_spot_price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
 
+###### aws security group datasource terraform  ######
+data "aws_security_group" "selected" {
+  id = "allow-all"
+}
+
+output "allow_all_id" {
+  value = data.aws_security_group.selected.id
+}
